18e6bb6d8a729911261e354c3718b2076764cd92
Divya Sankaran <dsankaran@newrelic.com>
2016-03-30 19:49:17 -0700

    creating empty rails app structure

---
+++ listings_web/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ListingsWeb::Application.config.secret_token = '7def184610532f4233efde8c2f05732919f6d0797c0976aec87600d98cc87b216b64af29907641507784eb7d71ff609af854b5ff124a9793ff7f0c7e8ff5acb9'
