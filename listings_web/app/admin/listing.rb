a07ee6685e52179c39b86cae1ff0c316e503c91c
Divya Sankaran <dsankaran@newrelic.com>
2016-03-31 21:13:21 -0700

    introducing the active admin gem for basic UI formatting

---
+++ listings_web/app/admin/listing.rb
@@ -0,0 +1,19 @@
+ActiveAdmin.register Listing do
+  permit_params :listing_identifier, :listing_source_url, :address
+  actions :all, except: [:update, :destroy]
+
+  config.filters = true
+  filter :listing_identifier
+  filter :listing_source_url
+
+  index do
+    column :listing_identifier, sortable: :listing_identifier
+    column :listing_source_url
+    column 'Address' do |listing|
+      link_to listing.address, admin_listing_path(listing)
+    end
+    actions
+  end
+
+  
+end
