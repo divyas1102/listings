8c491e04bccd012e82b441472e39b62b6e2025ed
Divya Sankaran <dsankaran@newrelic.com>
2016-03-31 21:15:54 -0700

    updating xml parser to gather all information from xml file

--- README.md
+++ README.md
@@ -16,6 +16,12 @@ A rails app that processes property listing information from an xml feed.
 3. user page to edit existing listing information (based on specific user type)
 
 #####Assumptions made:#####
-1. address = street, city, state and zipcode
+1. address is called a duplicate if latitude and longitude are the same
 2. if a listing has less than 3 photos, the listing is NOT saved in the database
 3. xml file is valid
+4. zipcode is valid and only US based (can be made a string to support other countries)
+5. landlord = agent
+
+#####Possible alterations:#####
+1. phone number can be stored as an integer (to add additional validation to the expected format), but the preferred practice is to use string types, since no mathematical actions will be performed on it.
+2. email and phone numbers could be in a different table (landord_details) by itself to allow multiple emails and phone numbers to be stored for each landlord.
--- listings_web/lib/xml_processor.rb
+++ listings_web/lib/xml_processor.rb
@@ -7,30 +7,96 @@ class XmlProcessor
 
     parsed_xml.xpath("//property").each do |property|
       current_address = nil
-      current_landlord = nil
+      current_landlord = nil        
 
       property.xpath('location').each do |address_element|
-        street = address_element.xpath('street-address').text
-        city = address_element.xpath('city-name').text
-        state = address_element.xpath('state-code').text
-        zipcode = address_element.xpath('zipcode').text
-        current_address = Address.new(street: street, city: city, state: state, zipcode: zipcode)
-        current_address.save!
+        address = {
+          street: address_element.xpath('street-address').text,
+          city: address_element.xpath('city-name').text,
+          state: address_element.xpath('state-code').text,
+          zipcode: address_element.xpath('zipcode').text
+        }
+
+        longitude = address_element.xpath('longitude').text
+        latitude = address_element.xpath('latitude').text
+
+        begin
+          current_address = Address.where(longitude: longitude, latitude: latitude).first_or_create!
+          current_address.update_attributes!(address)
+        rescue => e
+          # log e
+          puts "Create Address Error: " + e.message
+          next
+        end
       end
 
       property.xpath('agent').each do |landlord_element|
-        phone = landlord_element.xpath('agent-phone').text
-        email = landlord_element.xpath('agent-email').text
-        current_landlord = Landlord.new(phone: phone, email: email)
-        current_landlord.save!
+        name = landlord_element.xpath('agent-name').text
+
+        landlord = {            
+          phone: landlord_element.xpath('agent-phone').text,
+          email: landlord_element.xpath('agent-email').text
+        }
+
+        begin
+          current_landlord = Landlord.where(name: name).first_or_initialize
+          current_landlord.update_attributes!(landlord)
+        rescue => e
+          #log e
+          puts "Create Landord Error: " + e.message
+          next
+        end
       end
 
-      listing_id = property.xpath('provider-listingid').text
-      listing_source_url = property.xpath('lp-url').text
+      listing_identifier = property.xpath('details/provider-listingid').text
+      listing_source_url = property.xpath('landing-page/lp-url').text
 
-      Listing.new(listing_id: listing_id, listing_source_url: listing_source_url, address_id: current_address.id, landlord_id: current_landlord.id).save!
+      listing_details = {
+        price: property.xpath('details/price').text,
+        bathroom_count: property.xpath('details/num-bathrooms').text,
+        bedroom_count: property.xpath('details/num-bedrooms').text,
+        title: property.xpath('details/listing-title').text,
+        description: property.xpath('details/description').text
+      }
+
+      begin
+        current_listing = Listing.where(listing_identifier: listing_identifier, listing_source_url: listing_source_url).first_or_initialize
+
+        property.xpath('pictures').each do |photo_element|
+          url = photo_element.xpath('picture-url').text
+          photo = {           
+            caption: photo_element.xpath('picture-caption').text,
+            description: photo_element.xpath('picture-description').text,
+            seq: photo_element.xpath('picture-seq-number').text
+          }
+          all_photos = []
+          current_photo = Photo.where(url: url).first_or_initialize
+          current_photo.update_attributes!(photo)
+          all_photos << current_photo
+          current_listing.update_attributes!(address: current_address, landlord: current_landlord)
+          current_listing.save!
+
+          begin
+            current_listing_detail = ListingDetail.where(listing: current_listing).first_or_initialize
+            current_listing_detail.update_attributes!(listing_details)
+          rescue => e
+            # log e
+            puts "Create Listing Detail Error: " + e.message
+            next
+          end
+      
+          # if all_photos.length > 3
+          #   current_listing.update_attributes!(address: current_address, landlord: current_landlord)
+          #   all_photos.each do |p|
+          #     ListingPhoto.new(listing: current_listing, photo: p)
+          #   end
+          # end
+        end
+      rescue => e
+        # log e
+        puts "Create Listing Error: " + e.message
+        next
+      end
     end
   end
-
-  puts self.process
 end
